0b3a1b79b1746c51df876ada78e5a9a4605abfaf
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-09-11 10:59:16 -0300

    refactor(carMiddleware): move validation for required fields into middleware

--- middlewares/car.middleware.js
+++ middlewares/car.middleware.js
@@ -17,3 +17,42 @@ module.exports.checkID = async (req, res, next) => {
     res.status(500).json({ message: e.message });
   }
 };
+
+module.exports.checkRequiredFields = (req, res, next) => {
+  const { brand, model, year, items } = req.body;
+
+  if (!brand || brand.trim() === "") {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "brand is required" });
+  }
+
+  if (!model || model.trim() === "") {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "model is required" });
+  }
+
+  if (!year) {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "year is required" });
+  }
+
+  if (!items) {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "items is required" });
+  }
+
+  const currentYear = new Date().getFullYear() + 1;
+
+  if (year < currentYear - 10 || year > currentYear) {
+    return res.status(400).json({
+      status: "fail",
+      message: `year should be between ${currentYear - 10} and ${currentYear}`,
+    });
+  }
+
+  next();
+};
