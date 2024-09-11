9b7988ab6c655bcf2ba230721807301d8d689876
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-09-11 11:02:50 -0300

    refactor(carRouter): update post route to implement middleware for fields validation

--- routes/car.router.js
+++ routes/car.router.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 
 const controllers = require("../controllers/car.controller");
-const middlewares = require("../middlewares/car.middleware")
+const middlewares = require("../middlewares/car.middleware");
 
 const router = Router();
 
-router.param("id", middlewares.checkID)
+router.param("id", middlewares.checkID);
 
-router.route("/cars").get(controllers.findAll).post(controllers.save);
+router
+  .route("/cars")
+  .get(controllers.findAll)
+  .post(middlewares.checkRequiredFields, controllers.save);
 router
   .route("/cars/:id")
   .get(controllers.findByID)
