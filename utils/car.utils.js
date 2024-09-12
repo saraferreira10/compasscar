9e37c53d00dd0d5ba524b51040370c1788eea965
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-09-12 14:00:35 -0300

    fix(patchCar): fix logic of update items

--- utils/car.utils.js
+++ utils/car.utils.js
@@ -10,13 +10,40 @@ module.exports.validateCarYear = (year) => {
 
 module.exports.updateCarItems = async (id, items) => {
   try {
-    await db.execute("DELETE FROM cars_items WHERE car_id = ?", [id]); // deleta os itens antigos caso existam
+    let newItems = items; // armazena os itens que devem ser adicionados
+    let removeItems = []; // armazena os itens que devem ser removidos
 
-    for (const item of items) {
-      await db.execute("INSERT INTO cars_items (name, car_id)  VALUES (?, ?)", [
-        item,
-        id,
-      ]);
+    const [currentItems] = await db.execute(
+      `SELECT cars.id, group_concat(name) AS items FROM cars 
+        RIGHT JOIN cars_items ON cars_items.car_id = cars.id 
+        WHERE cars.id = ?
+        GROUP BY cars.id
+      `,
+      [id]
+    ); // retorna os itens já cadastrados
+
+    if (currentItems.length > 0) {
+      const oldItems = currentItems[0].items.split(","); // armazena em um array os itens já cadastrados
+
+      for (const old of oldItems) {
+        newItems = newItems.filter((item) => item != old); // retira os itens que já estão cadastrados e não devem ser excluídos
+        if (!items.includes(old)) removeItems.push(old); // adiciona em removeItems os itens que já estão cadastrados e devem ser removidos
+      }
+    }
+
+    if (removeItems.length > 0) {
+      for (const item of removeItems) {
+        await db.execute("DELETE FROM cars_items WHERE name = ?", [item]);
+      }
+    }
+
+    if (newItems.length > 0) {
+      for (const item of newItems) {
+        await db.execute(
+          "INSERT INTO cars_items (name, car_id) VALUES (?, ?)",
+          [item, id]
+        );
+      }
     }
   } catch (e) {
     console.error(e);
