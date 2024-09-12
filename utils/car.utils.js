3a519e4c9d02a6038030caf85a8af854b833fe68
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-09-12 16:18:37 -0300

    refactor(patchCar): remove updateCarItens utility function

--- utils/car.utils.js
+++ utils/car.utils.js
@@ -8,55 +8,19 @@ module.exports.validateCarYear = (year) => {
   };
 };
 
-module.exports.insertCarItems = async (id, items) => {
-  const uniqueItems = [...new Set(items)];
-
-  for (const item of uniqueItems) {
-    await db.execute("INSERT INTO cars_items (name, car_id) VALUES (?, ?)", [
-      item,
-      id,
-    ]);
-  }
-};
-
-module.exports.updateCarItems = async (id, items) => {
+module.exports.insertCarItems = async (connection, id, items) => {
   try {
-    let newItems = [...new Set(items)]; // armazena os itens que devem ser adicionados
-    let removeItems = []; // armazena os itens que devem ser removidos
+    const uniqueItems = [...new Set(items)];
 
-    const [currentItems] = await db.execute(
-      `SELECT cars.id, group_concat(name) AS items FROM cars 
-        RIGHT JOIN cars_items ON cars_items.car_id = cars.id 
-        WHERE cars.id = ?
-        GROUP BY cars.id
-      `,
-      [id]
-    ); // retorna os itens já cadastrados
+    await db.execute("DELETE FROM cars_items WHERE car_id = ?", [id]); // deleta os itens caso existam
 
-    if (currentItems.length > 0) {
-      const oldItems = currentItems[0].items.split(","); // armazena em um array os itens já cadastrados
-
-      for (const old of oldItems) {
-        newItems = newItems.filter((item) => item != old); // retira os itens que já estão cadastrados e não devem ser excluídos
-        if (!items.includes(old)) removeItems.push(old); // adiciona em removeItems os itens que já estão cadastrados e devem ser removidos
-      }
-    }
-
-    if (removeItems.length > 0) {
-      for (const item of removeItems) {
-        await db.execute("DELETE FROM cars_items WHERE name = ?", [item]);
-      }
-    }
-
-    if (newItems.length > 0) {
-      for (const item of newItems) {
-        await db.execute(
-          "INSERT INTO cars_items (name, car_id) VALUES (?, ?)",
-          [item, id]
-        );
-      }
+    for (const item of uniqueItems) {
+      await connection.execute(
+        "INSERT INTO cars_items (name, car_id) VALUES (?, ?)",
+        [item, id]
+      );
     }
   } catch (e) {
-    console.error(e);
+    throw e;
   }
 };
